c4571834d2ae988aee00336ec6d034b975c090dc
Neo <c.min.tseng@gmail.com>
2021-07-01 03:37:58 +0800

    modify buildscript dependencies let project like Android Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
 buildscript {
+    val ktor_version: String by project
+    val kotlin_version: String by project
+    val logback_version: String by project
+    val okta_jwt_verifier_version: String by project
+    extra.apply {
+        set("kotlin_version", kotlin_version)
+        set("ktor_version", ktor_version)
+        set("logback_version", logback_version)
+        set("okta_jwt_verifier_version", okta_jwt_verifier_version)
+    }
     repositories {
         mavenLocal()
         mavenCentral()
@@ -9,7 +19,7 @@ buildscript {
     }
 
     dependencies {
-
+        classpath(kotlin("gradle-plugin", rootProject.extra["kotlin_version"].toString()))
     }
 }
 
--- server/backend/build.gradle.kts
+++ server/backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm")
 }
 
 group = "com.okta.demo.ktor"
@@ -10,29 +10,18 @@ application {
     mainClassName = "io.ktor.server.cio.EngineMain"
 }
 
-//kotlin.sourceSets["main"].kotlin.srcDirs("src")
-//kotlin.sourceSets["test"].kotlin.srcDirs("test")
-//
-//sourceSets["main"].resources.srcDirs("resources")
-//sourceSets["test"].resources.srcDirs("testresources")
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val okta_jwt_verifier_version: String by project
-
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-cio:$ktor_version")
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-html-builder:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-auth:$ktor_version")
-    implementation("com.okta.jwt:okta-jwt-verifier:$okta_jwt_verifier_version")
-    implementation("com.okta.jwt:okta-jwt-verifier-impl:$okta_jwt_verifier_version")
+    implementation(kotlin("stdlib-jdk8", project.extra["kotlin_version"].toString()))
+    implementation("ch.qos.logback:logback-classic:${project.extra["logback_version"].toString()}")
+    implementation("io.ktor:ktor-server-cio:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-server-core:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-client-cio:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-html-builder:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-server-host-common:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-server-sessions:${project.extra["ktor_version"].toString()}")
+    implementation("io.ktor:ktor-auth:${project.extra["ktor_version"].toString()}")
+    implementation("com.okta.jwt:okta-jwt-verifier:${project.extra["okta_jwt_verifier_version"].toString()}")
+    implementation("com.okta.jwt:okta-jwt-verifier-impl:${project.extra["okta_jwt_verifier_version"].toString()}")
 }
 
 
